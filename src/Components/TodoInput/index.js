4c8581f83e72c344e2219edf3a7b478139099729
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-29 11:50:57 +0300

    create todo input component

---
+++ src/Components/TodoInput/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../Redux/actions';
+
+const TodoInput = () => {
+  const [task, setTask] = useState('');
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (e.target.task.value === '') {
+      return false;
+    } else {
+      dispatch(
+        addTodo({
+          name: task,
+        })
+      );
+      setTask('');
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField type="text" id="task" placeholder='Create a new todo...' value={task} onChange={handleChange} />
+    </form>
+  );
+};
+
+export default TodoInput;
